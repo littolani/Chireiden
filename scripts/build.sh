1e85493b1de5555b05aa3a610fa3dd70242176c6
littolani <sparkra1n@proton.me>
2026-07-14 21:05:40 -0700

    Make build script generate definitions for globals

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,5 @@
+echo "Regenerating global variables..."
+python ./scripts/GenerateHeaders.py
+echo "Running Docker..."
 docker run --rm --user $(id -u):$(id -g) -v ${PWD}:/workspace touhou-builder python3 scripts/GenerateVfs.py /workspace/msvc_env
 docker run --rm --user $(id -u):$(id -g) -v ${PWD}:/workspace touhou-builder
